c9b8dd02b2b8789f2e12b8c42abe7eff0aa274ec
damang27 <49058836+damang27@users.noreply.github.com>
2019-05-16 08:16:30 +0800

    Add files via upload

---
+++ bch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+clear
+pkg install update
+pkg install upgrade
+apt update
+apt upgrade -y
+pkg install python -y
+pip --upgrade pip
+pip install -r requirements.txt
+
+echo
+echo -e "\e[1n Subscribe to Termux Tricks & Tut"
+echo
+echo -e "\e[1m\e[32m Enter :\e[33m python main.py (country_code_area_code_number)"
+echo
+echo -e "\e[1m\e[32m Example :\e[33m python main.py +639359094586"
+echo
+echo
